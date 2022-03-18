33070dcff3167d1062ac325a62c59a04b9e9494b
Lena Mouawad <lena.mouawad@hotmail.com>
2022-03-18 08:29:30 +0100

    Page favoris

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticlesFavorisComponent } from './pages/articles-favoris/articles-favoris.component';
 import { MaTenueComponent } from './pages/ma-tenue/ma-tenue.component';
 import { MonDressingComponent } from './pages/mon-dressing/mon-dressing.component';
 import { MonProfilComponent } from './pages/mon-profil/mon-profil.component';
@@ -9,6 +10,7 @@ import { PageVoyageComponent } from './pages/page-voyage/page-voyage.component';
 const routes: Routes = [
   {path:"", component: PageAcceuilComponent},
   {path:"monDressing", component: MonDressingComponent},
+  {path:"favoris", component: ArticlesFavorisComponent},
   {path:"maTenue", component: MaTenueComponent},
   {path:"voyage", component: PageVoyageComponent},
   {path:"profil", component: MonProfilComponent},
