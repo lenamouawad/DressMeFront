6961ea409d4b9984a14ba46b3ced553b6ed2eefe
Lena Mouawad <lena.mouawad@hotmail.com>
2022-03-19 20:17:51 +0100

    Filtres Hauts et Bas - pages Dressing et Favoris

--- src/app/pages/articles-favoris/articles-favoris.component.ts
+++ src/app/pages/articles-favoris/articles-favoris.component.ts
@@ -14,18 +14,75 @@ export class ArticlesFavorisComponent implements OnInit {
   hauts:Haut[];
   bas:Bas [];
 
+  hautsToShow:Haut[];
+  basToShow:Bas[];
+
+  whatToShow : number;
+
   constructor(private hautService : HautService, private basService : BasService) { }
 
   ngOnInit(): void {
     this.initHauts();
     this.initBas();
+    this.initImagesToShow();
+
+    this.initWhatToShow();
   }
 
   initHauts(){
-    this.hautService.findAllFavoris().subscribe(data => {this.hauts = data.slice(0,8);});
+    this.hautService.findAllFavoris().subscribe(data => {this.hauts = data;});
   }
   initBas(){
-    this.basService.findAllFavoris().subscribe(data => {this.bas = data.slice(0,8);});
+    this.basService.findAllFavoris().subscribe(data => {this.bas = data;});
   }
+  initImagesToShow(){    
+    this.hautService.findAllFavoris().subscribe(data => {this.hautsToShow = data.slice(0,8);});
+    this.basService.findAllFavoris().subscribe(data => {this.basToShow = data.slice(0,8);});
+
+    if (this.hauts.length>8) /* length doesnt work on property... */
+     {
+    this.hautService.findAllFavoris().subscribe(data => {this.hautsToShow = data.slice(0,8);});
+      }
+  else
+  {
+    this.hautsToShow = this.hauts;
+  }
+  if (this.bas.length>8)
+     {
+      this.basService.findAllFavoris().subscribe(data => {this.basToShow = data.slice(0,8);});
+    }
+  else
+  {
+    this.basToShow =this.bas;
+  }
+}
+  initWhatToShow(){this.showImages(0);}
+
+   showArrows() : boolean{
+    if (this.hauts.length > 8){  /* a changer -> all hauts et bas*/
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  showImages(categorie : number) {
+    var nbr = 0;
+    if (categorie==0)
+    {
+      nbr = 0;
+    }
+    else if (categorie==1)
+    {
+      nbr =  1;
+    }
+    else if (categorie==2)
+    {
+      nbr =2;
+    }
+    this.whatToShow = nbr;
+  }
+
 
 }
